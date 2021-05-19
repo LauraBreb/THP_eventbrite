81c168a58a1ebf12d54bdfddf5c8f4026929a2d2
LauraBreb <breb.laura@gmail.com>
2021-05-19 14:21:46 +0200

    connexion

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :events
-  root to: "events#index"
   devise_for :users
-  resources :users, only: [:edit, :update, :show]
+  resources :users
+  resources :events
+  root "events#index"
 end
